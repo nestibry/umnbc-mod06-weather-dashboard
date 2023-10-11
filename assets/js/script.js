1d2de7232c2839d4ebcc9afe80240740dcef1198
nestibry <nestibry@gmail.com>
2023-10-11 16:18:31 -0500

    added to-do list on the script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+
+    Order of Operations
+    1. Create a blank HTML and Play around with OpenWeatherAPI
+        https://openweathermap.org/forecast5
+        https://openweathermap.org/api/geocoding-api
+    2. Bootstrap a html template
+        include mostly the same with these additions if possible
+        a. Map with radar
+        b. 3-hour rows for each day
+    3. Write jQuery code to dynamically update content
+
+*/
