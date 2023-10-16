0ea7c8d2d3d81c3bb39c08b6185ddfc828f78e5d
nestibry <nestibry@gmail.com>
2023-10-16 09:45:08 -0500

    added geocoding search (max limit 5 per API docs) when user uses search bar

--- assets/js/test-modal-form.js
+++ assets/js/test-modal-form.js
@@ -1,38 +1,54 @@
 // Global Variables
 var apiKey = "8126bb2957be37f081cd3c30e29ee1f6";
-var limit = 5;
-
 var geoData = [];
 var forecastData = [];
 
+// Location Search
+var locationInput = "";
+var locationSearchEl = $('#location-search');
+var locationInputEl = $('#location-input');
 
-// Geocoding by City, (State), Country Code
-var city = "chaseburg,wisconsin,us";
-var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`;
-console.log(geoUrl);
 
-fetch(geoUrl)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    geoData = data[0];
 
-    // OpenWeather API 5-day/3-hour Weather Forecasting
-    var lat = geoData.lat; 
-    var lon = geoData.lon;
-    var units = "imperial"
-    var baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-    console.log(baseUrl);
+locationSearchEl.on('submit', function(event){
+    
+    event.preventDefault();
+    event.stopPropagation();
 
-    fetch(baseUrl)
+    // User input location, City or Zipcode
+    locationInput = locationInputEl.val();
+    console.log(`Searching for: ${locationInput}`);
+
+    // Geocoding by City, (State), Country Code  // Max return limit is 5
+    var city = locationInput;
+    var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+    console.log(geoUrl);
+    
+    fetch(geoUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
-        forecastData = data;
+        geoData = data;
+
+        //     // // OpenWeather API 5-day/3-hour Weather Forecasting
+        //     // var lat = geoData.lat; 
+        //     // var lon = geoData.lon;
+        //     // var units = "imperial"
+        //     // var baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+        //     // console.log(baseUrl);
+
+        //     // fetch(baseUrl)
+        //     // .then(function (response) {
+        //     //     return response.json();
+        //     // })
+        //     // .then(function (data) {
+        //     //     console.log(data);
+        //     //     forecastData = data;
+        //     // });
+
     });
+
 
 });
